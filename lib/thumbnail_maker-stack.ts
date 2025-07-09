f9aa7c731e737d42f375c96b2bd68a2e472938f0
Anthony <anthonyhaughton52@gmail.com>
2025-07-09 18:54:13 -0400

    create public S3 bucket

--- lib/thumbnail_maker-stack.ts
+++ lib/thumbnail_maker-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import { S3 } from 'aws-cdk-lib/aws-ses-actions';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ThumbnailMakerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ThumbnailMakerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, 'Bucket', {
+      bucketName: 'ingest-image-bucket',
+      publicReadAccess: true,
+      blockPublicAccess: {
+        blockPublicAcls: false,
+				blockPublicPolicy: false,
+			  ignorePublicAcls: false,
+				restrictPublicBuckets: false,
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
   }
 }
