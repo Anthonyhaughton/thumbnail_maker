9dfb00ba16a12bf7c40a33471ef84260e7c7ab83
Anthony <anthonyhaughton52@gmail.com>
2025-07-09 18:27:23 -0400

    init cdk app

---
+++ test/thumbnail_maker.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ThumbnailMaker from '../lib/thumbnail_maker-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/thumbnail_maker-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ThumbnailMaker.ThumbnailMakerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
