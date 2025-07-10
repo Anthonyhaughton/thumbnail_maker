8f03a73c333c4fac873ed99bf6904777353b042d
Anthony <anthonyhaughton52@gmail.com>
2025-07-09 20:08:29 -0400

    created ingress and egress bucket

--- lib/thumbnail_maker-stack.ts
+++ lib/thumbnail_maker-stack.ts
@@ -3,22 +3,51 @@ import { S3 } from 'aws-cdk-lib/aws-ses-actions';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { version } from 'process';
+import { S3Bucket } from 'aws-cdk-lib/aws-kinesisfirehose';
 
 export class ThumbnailMakerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-    new s3.Bucket(this, 'Bucket', {
-      bucketName: 'ingest-image-bucket',
+    // Create public Bucket for image upload
+    const IngressBucket = new s3.Bucket(this, 'IngressBucket', {
+      bucketName: 'haughton-upload-image',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
       publicReadAccess: true,
+      versioned: true,
       blockPublicAccess: {
         blockPublicAcls: false,
-				blockPublicPolicy: false,
-			  ignorePublicAcls: false,
-				restrictPublicBuckets: false,
-      },
-      removalPolicy: cdk.RemovalPolicy.DESTROY
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      }
+    });
+
+    // Create public Bucket for processed images
+    const EgressBucket = new s3.Bucket(this, 'EgressBucket', {
+      bucketName: 'haughton-processed-image',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: true,
+      versioned: true,
+      blockPublicAccess: {
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      }
+    });
+
+    // Output Ingress bucket name
+    new cdk.CfnOutput(this, 'IngressBucketName', {
+      value: IngressBucket.bucketName
+    });
+
+    // Output Egress bucket name
+    new cdk.CfnOutput(this, 'EgressBucketName', {
+      value: EgressBucket.bucketName
     });
   }
 }
